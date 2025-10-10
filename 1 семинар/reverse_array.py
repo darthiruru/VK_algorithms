d46a716357cff4e024488910d5d7102984016619
Iruru <i.gribkina@icloud.com>
2025-10-10 13:42:58 +0200

    Change reverse_array task

--- 1 семинар/reverse_array.py
+++ 1 семинар/reverse_array.py
@@ -6,13 +6,7 @@ def reverseArray(a):
         right -= 1
 
 
-def reverseArray_pythonic(a):
-    return a[::-1]
-
-
 if __name__ == "__main__":
     a = list(map(int, input().split()))
     reverseArray(a)
-    print(*a)
-    a = reverseArray_pythonic(a)
     print(*a)
