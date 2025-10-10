9d64d8c4bb3f6b92ff7f5091f67b9f8cf9d7dc82
Iruru <i.gribkina@icloud.com>
2025-10-10 13:15:20 +0200

    Add reverse_array task

--- 1 семинар/reverse_array.py
+++ 1 семинар/reverse_array.py
@@ -1,12 +1,13 @@
 def reverseArray(a):
+    res = a[::]
     left, right = 0, len(a) - 1
     while left < right:
-        a[left], a[right] = a[right], a[left]
+        res[left], res[right] = res[right], res[left]
         left += 1
         right -= 1
+    return res
 
 
 if __name__ == "__main__":
     a = list(map(int, input().split()))
-    reverseArray(a)
-    print(*a)
+    print(*reverseArray(a))
