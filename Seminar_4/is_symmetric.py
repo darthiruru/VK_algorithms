828d34c5d7aa110fe07ae46f044e14c4bdc40728
darth_iruru <i.gribkina@gmail.com>
2025-11-18 02:02:57 +0300

    Add is_symmetric task

---
+++ Seminar_4/is_symmetric.py
@@ -0,0 +1,98 @@
+class TreeNode:
+    def __init__(self, val: int=0, left: TreeNode=None, right: TreeNode=None) -> None:
+        self.val = val
+        self.left = left
+        self.right = right
+
+def is_symmetric(root: TreeNode) -> bool:
+    if root is None:
+        return True
+    queue = [root]
+    while len(queue) > 0:
+        queue_len = len(queue)
+        for i in range(queue_len // 2):
+            left = queue[i]
+            right = queue[-i - 1]
+            if left is None and right is None:
+                continue
+            if left is None or right is None:
+                return False
+            if left.val != right.val:
+                return False
+        next_level = []
+        for node in queue:
+            if node is not None:
+                next_level.append(node.left)
+                next_level.append(node.right)
+        queue = next_level
+    return True
+
+
+# Я дополнила depth_search, так как версия из презентации некорректно отрабатывает для деревьев вида
+# 1
+#  \
+#   1
+#    \
+#     1
+def depth_search(root: TreeNode, res: list[int]) -> None:
+    if root is None:
+        res.append(None)
+        return
+    depth_search(root.left, res)
+    res.append(root.val)
+    depth_search(root.right, res)
+    if res[-3] is None and res[-1] is None:
+        del res[-3]
+        del res[-1]
+    
+def is_symmetric_dfs(root: TreeNode) -> bool:
+    if root is None:
+        return True
+    data = []
+    depth_search(root, data)
+    if data == data[::-1]:
+        return True
+    return False
+    
+
+if __name__ == "__main__":
+    assert is_symmetric(None) == True
+    assert is_symmetric_dfs(None) == True
+    assert is_symmetric(TreeNode(1)) == True
+    assert is_symmetric_dfs(TreeNode(1)) == True
+    root = TreeNode(1, TreeNode(2), None)
+    assert is_symmetric(root) == False
+    assert is_symmetric_dfs(root) == False
+    root = TreeNode(1, TreeNode(2), TreeNode(2))
+    assert is_symmetric(root) == True
+    assert is_symmetric_dfs(root) == True
+    root = TreeNode(1,
+        TreeNode(2, None, TreeNode(3)),
+        TreeNode(2, TreeNode(3), None)
+    )
+    assert is_symmetric(root) == True
+    assert is_symmetric_dfs(root) == True
+    root = TreeNode(1,
+        TreeNode(2, None, TreeNode(3)),
+        TreeNode(2, None, TreeNode(3))
+    )
+    assert is_symmetric(root) == False
+    assert is_symmetric_dfs(root) == False
+    root = TreeNode(1, TreeNode(2), TreeNode(3))
+    assert is_symmetric(root) == False
+    assert is_symmetric_dfs(root) == False
+    root = TreeNode(1,
+        TreeNode(2, TreeNode(3), None),
+        TreeNode(2, None, TreeNode(4))
+    )
+    assert is_symmetric(root) == False
+    assert is_symmetric_dfs(root) == False
+    root = TreeNode(1,
+        TreeNode(2, TreeNode(3), None),
+        None
+    )
+    assert is_symmetric(root) == False
+    assert is_symmetric_dfs(root) == False
+    root = TreeNode(1, None, TreeNode(1, None, TreeNode(1)))
+    assert is_symmetric(root) == False
+    assert is_symmetric_dfs(root) == False
