1b4258f26f13b14fcdd50bbda970f0c936a11ae2
darth_iruru <i.gribkina@gmail.com>
2025-11-17 19:24:33 +0300

    Add build_tree task

---
+++ Seminar_4/build_tree.py
@@ -0,0 +1,28 @@
+class TreeNode:
+    def __init__(self, val: int=0, left: TreeNode=None, right: TreeNode=None) -> None:
+        self.val = val
+        self.left = left
+        self.right = right
+
+def build_tree(arr, i=0):
+    if i >= len(arr):
+        return None
+    root = TreeNode(arr[i])
+    root.left = build_tree(arr, 2 * i + 1)
+    root.right = build_tree(arr, 2 * i + 2)
+    return root
+
+
+if __name__ == "__main__":
+    assert build_tree([]) is None
+    r = build_tree([5])
+    assert r.val == 5 and r.left is None and r.right is None
+    arr = [1,2,3,4,5,6,7]
+    r = build_tree(arr)
+    assert r.val == 1
+    assert r.left.val == 2
+    assert r.right.val == 3
+    assert r.left.left.val == 4
+    assert r.left.right.val == 5
+    assert r.right.left.val == 6
+    assert r.right.right.val == 7
