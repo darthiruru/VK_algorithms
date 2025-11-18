1650c3943c69f0230f05c7122a2eabca9bb7defa
darth_iruru <i.gribkina@gmail.com>
2025-11-18 14:28:12 +0300

    Add msx_min_multiplication task

---
+++ Seminar_4/max_min_multiplication.py
@@ -0,0 +1,17 @@
+def max_min_multiplication(data):
+    if len(data) < 3:
+        return -1
+    i = 1
+    while 2 * i + 1 < len(data):
+        i = 2 * i + 1
+    min_index = i
+    i = 2
+    while 2 * i + 2 < len(data):
+        i = 2 * i + 2
+    max_index = i
+    return data[min_index] * data[max_index]
+
+
+if __name__ == '__main__':
+    a = list(map(int, input().split()))
+    print(max_min_multiplication(a))
