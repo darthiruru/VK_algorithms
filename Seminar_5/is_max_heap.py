ffb7ca182d30e7c26d7ee0b8d366e3167dc934d7
darth_iruru <i.gribkina@gmail.com>
2025-11-25 15:58:27 +0300

    Add is_max_heap task

---
+++ Seminar_5/is_max_heap.py
@@ -0,0 +1,49 @@
+def is_max_heap(arr: list[int]) -> bool:
+    n = len(arr)
+    for i in range((n - 2) // 2 + 1):
+        left = 2 * i + 1
+        right = 2 * i + 2
+        if left < n and arr[i] < arr[left]:
+            return False
+        if right < n and arr[i] < arr[right]:
+            return False
+    return True
+
+def is_max_heap_bfs(arr: list[int]) -> bool:
+    n = len(arr)
+    if n <= 1:
+        return True
+    queue = []
+    queue.append(0)
+    while (len(queue) > 0):
+        i = queue.pop(0)
+        left = 2 * i + 1
+        right = 2 * i + 2
+        if left < n:
+            if arr[i] < arr[left]:
+                return False
+            queue.append(left)
+        if right < n:
+            if arr[i] < arr[right]:
+                return False
+            queue.append(right)
+    return True
+
+
+if __name__ == '__main__':
+    assert is_max_heap([]) is True
+    assert is_max_heap_bfs([]) is True
+    assert is_max_heap([42]) is True
+    assert is_max_heap_bfs([42]) is True
+    assert is_max_heap([10, 9, 8, 7, 6, 5, 4]) is True
+    assert is_max_heap_bfs([10, 9, 8, 7, 6, 5, 4]) is True
+    assert is_max_heap([5, 5, 5, 5, 5]) is True
+    assert is_max_heap_bfs([5, 5, 5, 5, 5]) is True
+    assert is_max_heap([20, 15, 18, 10, 12]) is True
+    assert is_max_heap_bfs([20, 15, 18, 10, 12]) is True
+    assert is_max_heap([10, 20, 5]) is False
+    assert is_max_heap_bfs([10, 20, 5]) is False
+    assert is_max_heap([10, 5, 30]) is False 
+    assert is_max_heap_bfs([10, 5, 30]) is False 
+    assert is_max_heap([40, 30, 20, 10, 50]) is False
+    assert is_max_heap_bfs([40, 30, 20, 10, 50]) is False
